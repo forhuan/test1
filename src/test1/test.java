ad066036770527180f84e949a6eaada51279267a
test <771773847@qq.com>
2016-10-25 19:42:27 +0800

    tcp server

--- src/test1/test.java
+++ src/test1/test.java
@@ -1,11 +1,26 @@
 package test1;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		// TODO Auto-generated method stub
 		System.out.println("HelloWorld");
+		try {
+			ServerSocket serverSocket = new ServerSocket(9876);
+			while(true){			
+				Socket socket = serverSocket.accept();
+				System.out.println(socket.getInetAddress().getHostAddress());
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+			System.exit(1);
+		}
 		
 	}
 }
